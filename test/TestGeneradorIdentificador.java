eb184b4860fcb0fcadb221846af496083052eb67
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-04 00:08:42 +0100

    Tests GeneradorIdentificador:     - Trackeado la clase esqueleto de pruebas de la clase

--- test/TestGeneradorIdentificador.java
+++ test/TestGeneradorIdentificador.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /**
  * CLASE DE TESTEO de la clase GeneradorIdentificador.
