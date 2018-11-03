9a1785fefe344960537140efdd0bc373e6f577d9
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-03 21:51:11 +0100

    Tests Identificador:     - Trackeada la clase esqueleto para las pruebas de la clase

---
+++ Test/TestIdentificador.java
@@ -0,0 +1,13 @@
+/**
+ * CLASE DE TESTEO de la clase Identificador.
+ *
+ * Realiza las pruebas de todos los métodos públicos de la clase para todas
+ * sus posibles entradas y estados
+ *
+ * @author : Juan Pablo García Plaza Pérez - Jose Ángel Concha Carrasco
+ * @grupo : Wild True
+ */
+
+public class TestIdentificador {
+
+}
