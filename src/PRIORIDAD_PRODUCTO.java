f3b2c3307850b16a015a7e2516fe717e7e483487
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-03 01:59:57 +0100

    Enum PRIORIDAD_PRODUCTO:     - Añadida la documentación a la clase

--- src/PRIORIDAD_PRODUCTO.java
+++ src/PRIORIDAD_PRODUCTO.java
@@ -1,3 +1,12 @@
+/**
+ * Tipo enumerado que contiene valores estandarizados de las distintas prioridades
+ * de reabastecimiento de los productos. Se orgranizan en 3 valores, que son asignados
+ * por constantes en la clase Producto
+ *
+ * @author : Juan Pablo García Plaza Pérez - Jose Ángel Concha Carrasco
+ * @grupo : Wild True
+ */
+
 public enum PRIORIDAD_PRODUCTO {
     BAJA,
     MEDIA,
