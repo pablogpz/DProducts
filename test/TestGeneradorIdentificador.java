88e2fff2b3ac1c25c96c548abb9889a94b8f2afa
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-03 23:28:17 +0100

    Tests GeneradorIdentificador:     - Trackeado la clase esqueleto de pruebas de la clase

---
+++ test/TestGeneradorIdentificador.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * CLASE DE TESTEO de la clase GeneradorIdentificador.
+ * <p>
+ * Realiza las pruebas de todos los métodos públicos de la clase para todas
+ * sus posibles entradas y estados
+ *
+ * @author : Juan Pablo García Plaza Pérez - Jose Ángel Concha Carrasco
+ * @grupo : Wild True
+ */
+
+public class TestGeneradorIdentificador {
+
+    /**
+     * Constructor privado. No hace falta instanciar la clase
+     */
+    private TestGeneradorIdentificador() {}
+
+    @BeforeAll
+    static void setUp() {
+
+    }
+
+}
