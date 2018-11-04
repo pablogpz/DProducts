9fbb477fc9486433665ca74f8198b2729180fde8
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-04 14:42:47 +0100

    Test Producto:     - Trackeada la clase esqueleto de tests de clase

---
+++ test/ProductoTest.java
@@ -0,0 +1,69 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * CLASE DE TESTEO de la clase Producto.
+ * <p>
+ * Realiza las pruebas de todos los métodos públicos de la clase para todas
+ * sus posibles entradas y estados
+ *
+ * @author : Juan Pablo García Plaza Pérez - Jose Ángel Concha Carrasco
+ * @grupo : Wild True
+ */
+
+public class ProductoTest {
+
+    // Fixture de objetos Producto de prueba
+
+    @BeforeClass
+    public static void setUp() {
+    }
+
+    @Test
+    public void getNombre() {
+    }
+
+    @Test
+    public void getIdentificador() {
+    }
+
+    @Test
+    public void getCantidad() {
+    }
+
+    @Test
+    public void getStockMinimo() {
+    }
+
+    @Test
+    public void getPrioridad() {
+    }
+
+    @Test
+    public void getFrabricante() {
+    }
+
+    @Test
+    public void getFechaLanzamiento() {
+    }
+
+    @Test
+    public void getEsReacondicionado() {
+    }
+
+    @Test
+    public void pedir() {
+    }
+
+    @Test
+    public void comentar() {
+    }
+
+    @Test
+    public void haySuficienteStock() {
+    }
+
+    @Test
+    public void detalles() {
+    }
+}
