63614aa4c60842a0e6156c745dcf51a858c75c9b
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-04 15:14:46 +0100

    Test Producto:     - Implementados los tests de los métodos accesores

--- test/ProductoTest.java
+++ test/ProductoTest.java
@@ -1,6 +1,10 @@
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
 /**
  * CLASE DE TESTEO de la clase Producto.
  * <p>
@@ -14,41 +18,76 @@ import org.junit.Test;
 public class ProductoTest {
 
     // Fixture de objetos Producto de prueba
+    private static Producto producto;
 
     @BeforeClass
     public static void setUp() {
+        producto = new Producto("Nombre", 30, 25, FABRICANTES.ACER, PRIORIDAD_PRODUCTO.MEDIA,
+                new GregorianCalendar(2011, 3, 26), true);
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'nombre'
+     */
     @Test
     public void getNombre() {
+        assertEquals("Nombre", producto.getNombre());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'identificador'
+     */
     @Test
     public void getIdentificador() {
+        assertNotNull(producto.getIdentificador());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'cantidad'
+     */
     @Test
     public void getCantidad() {
+        assertEquals(30, producto.getCantidad());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'stockMínimo'
+     */
     @Test
     public void getStockMinimo() {
+        assertEquals(25, producto.getStockMinimo());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'prioridad'
+     */
     @Test
     public void getPrioridad() {
+        assertEquals(PRIORIDAD_PRODUCTO.MEDIA, producto.getPrioridad());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'fabricante'
+     */
     @Test
     public void getFrabricante() {
+        assertEquals(FABRICANTES.ACER, producto.getFrabricante());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'fechaLanzamiento'
+     */
     @Test
     public void getFechaLanzamiento() {
+        assertEquals(new GregorianCalendar(2011, 3, 26), producto.getFechaLanzamiento());
     }
 
+    /**
+     * Testeo del método accesor del atribtuo 'esReacondicionado'
+     */
     @Test
     public void getEsReacondicionado() {
+        assertTrue(producto.getEsReacondicionado());
     }
 
     @Test
