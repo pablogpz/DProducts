461b008f4f9042355a175e1b87470993c2e5cb0f
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-04 14:36:57 +0100

    Test Comentario:     - Implementados todos los tests

--- test/ComentarioTest.java
+++ test/ComentarioTest.java
@@ -1,5 +1,8 @@
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 /**
  * CLASE DE TESTEO de la clase Comentario.
@@ -16,23 +19,43 @@ public class ComentarioTest {
     // Fixture de objetos Comentario de prueba
     private static Comentario comentario;
 
-    @Before
-    public void setUp() {
+    // Cliente de ejemplo
+    private static Cliente cliente;
+
+    @BeforeClass
+    public static void setUp() {
+        cliente = new Cliente("Nombre", 18, "Localidad");
+        comentario = new Comentario(cliente, "Test", 5);
+    }
+
+    /**
+     * Testea la excepción generada por el constructor al pasar parámetros no válidos
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void parametrosValidos() {
+        comentario = new Comentario(cliente, "", 6);
     }
 
     @Test
     public void getAutor() {
+        assertSame(cliente, comentario.getAutor());
     }
 
     @Test
     public void getTexto() {
+        assertEquals("Test", comentario.getTexto());
     }
 
     @Test
     public void getPuntuacion() {
+        assertEquals(5, comentario.getPuntuacion());
     }
 
     @Test
     public void comentarioCompleto() {
+        assertEquals("Autor : Nombre\n" +
+                "Calificación *****\n" +
+                "\tReseña :\n" +
+                "Test", comentario.comentarioCompleto());
     }
 }
