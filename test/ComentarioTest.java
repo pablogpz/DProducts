5a0a8b39090e3a0fc17386104a5d83c5d3e04088
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-04 01:10:00 +0100

    Test Comentario:     - Trackeada la clase esqueleto de las pruebas de la clase

---
+++ test/ComentarioTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * CLASE DE TESTEO de la clase Comentario.
+ * <p>
+ * Realiza las pruebas de todos los métodos públicos de la clase para todas
+ * sus posibles entradas y estados
+ *
+ * @author : Juan Pablo García Plaza Pérez - Jose Ángel Concha Carrasco
+ * @grupo : Wild True
+ */
+
+public class ComentarioTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void getAutor() {
+    }
+
+    @Test
+    public void getTexto() {
+    }
+
+    @Test
+    public void getPuntuacion() {
+    }
+
+    @Test
+    public void comentarioCompleto() {
+    }
+}
